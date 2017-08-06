0c96a2caebb8061b2acd99d8c62b81d95f71cf23
Daniel Somerfield <dsomerfi@thoughtworks.com>
2017-08-05 20:26:52 -0700

    [#21] functional validator.

--- server/service/crypto/crypto_tools.go
+++ server/service/crypto/crypto_tools.go
@@ -1,44 +1,59 @@
 package crypto
 
+import "github.com/danielsomerfield/authful/util"
+import (
+	"golang.org/x/crypto/scrypt"
+	"log"
+	"regexp"
+	"encoding/base64"
+
+	"bytes"
+	"strconv"
+	"math"
+)
+
 type BuildPwdHash func(string) []byte
 
-//func ScryptHash(input string) string {
-//	N := 16384
-//	r := 8
-//	p := 1
-//	salt := util.GenerateRandomBytes(16)
-//	keyLen := 32
-//
-//	hash, err := scrypt.Key([]byte(input), salt, N, r, p, keyLen)
-//
-//	if err != nil {
-//		log.Panic("Bad configuration of scrypt.")
-//	}
-//
-//	return string(hash)
-//}
-//
-//func ValidateScrypt(data string, hash string) bool {
-//
-//	re := regexp.MustCompile("\\$scrypt\\$ln=([0-9]+),r=([0-9]+),p=([0-9]+)\\$([a-zA-Z0-9]+)\\$([a-zA-Z0-9\\+]+).*")
-//
-//	matches := re.FindAllStringSubmatch(hash, -1)
-//	if len(matches) != 1 {
-//		return false
-//	}
-//
-//	//TODO: handle excessively large input numbers
-//	m := matches[0]
-//	ln, _ := strconv.ParseInt(m[1], 10, 32)
-//	r, _ := strconv.ParseInt(m[2], 10, 32)
-//	p, _ := strconv.ParseInt(m[3], 10, 32)
-//	salt := m[4]
-//	checksum := m[5]
-//
-//	saltBytes, err := base64.StdEncoding.DecodeString(salt)
-//	hashBytes, err := base64.StdEncoding.DecodeString(checksum)
-//
-//	key, err := scrypt.Key([]byte(data), saltBytes, int(ln), int(r), int(p), 32)
-//
-//	return err != nil && bytes.Equal(hashBytes, key)
-//}
+func ScryptHash(input string) string {
+	N := 16384
+	r := 8
+	p := 1
+	salt := util.GenerateRandomBytes(16)
+	keyLen := 32
+
+	hash, err := scrypt.Key([]byte(input), salt, N, r, p, keyLen)
+
+	if err != nil {
+		log.Panic("Bad configuration of scrypt.")
+	}
+
+	return string(hash)
+}
+
+func ValidateScrypt(data string, hash string) bool {
+
+	re := regexp.MustCompile("\\$scrypt\\$ln=([0-9]+),r=([0-9]+),p=([0-9]+)\\$([a-zA-Z0-9/]+)\\$([a-zA-Z0-9+/]+).*")
+
+	matches := re.FindAllStringSubmatch(hash, -1)
+	if len(matches) != 1 {
+		return false
+	}
+
+	//TODO: handle excessively large input numbers
+
+	m := matches[0]
+	ln, _ := strconv.ParseInt(m[1], 10, 32)
+	r, _ := strconv.ParseInt(m[2], 10, 32)
+	p, _ := strconv.ParseInt(m[3], 10, 32)
+	salt := m[4]
+	checksum := m[5]
+
+	saltBytes, err := base64.RawStdEncoding.DecodeString(salt)
+	checksumBytes, err := base64.RawStdEncoding.DecodeString(checksum)
+
+	key, err := scrypt.Key([]byte(data), saltBytes, int(math.Pow(2, float64(ln))), int(r), int(p), 32)
+
+	equal := bytes.Equal(checksumBytes, key)
+
+	return err == nil && equal
+}
--- server/service/crypto/crypto_tools_test.go
+++ server/service/crypto/crypto_tools_test.go
@@ -1,5 +1,11 @@
 package crypto
 
+import (
+	"testing"
+	"github.com/danielsomerfield/authful/util"
+	"strings"
+)
+
 //func TestScryptHash_buildsValidHash(t *testing.T) {
 //	hash := ScryptHash("foo")
 //	util.AssertTrue(ValidateScrypt("foo", hash), "the hash is valid", t)
@@ -18,9 +24,9 @@ package crypto
 //	util.AssertFalse(string(hash1) == string(hash2), "The hashes are equal", t)
 //}
 
-//func TestScryptValidator(t *testing.T) {
-//	hash := "$scrypt$ln=16,r=8,p=1$aM15713r3Xsvxbi31lqr1Q$nFNh2CVHVjNldFVKDHDlm4CbdRSCdEBsjjJxD+iCs5E"
-//	util.AssertTrue(ValidateScrypt("password", hash), "The hash is valid", t)
-//	//util.AssertFalse(ValidateScrypt("password2", hash), "The hash is valid", t)
-//	//util.AssertFalse(ValidateScrypt("password", strings.Replace(hash, "jjJ", "JJJ", 0)), "The hash is valid", t)
-//}
+func TestScryptValidator(t *testing.T) {
+	hash := "$scrypt$ln=16,r=8,p=1$aM15713r3Xsvxbi31lqr1Q$nFNh2CVHVjNldFVKDHDlm4CbdRSCdEBsjjJxD+iCs5E"
+	util.AssertTrue(ValidateScrypt("password", hash), "The hash is valid", t)
+	util.AssertFalse(ValidateScrypt("password2", hash), "The hash is valid", t)
+	util.AssertFalse(ValidateScrypt("password", strings.Replace(hash, "jjJ", "MMM", -1)), "The hash is valid", t)
+}
